20c1be9583ffe6bd9d40c51b9e062df7a083b8ee
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2025-12-10 22:48:12 +0600

    Started

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,15 +7,66 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    let trackId: Int
+    let trackName: String
+    let collectionName: String
+    let artworkUrl100: String
+}
+
 struct ContentView: View {
+    @State private var results = [Result]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(results, id: \.trackId) { result in
+            VStack(alignment: .leading) {
+                HStack {
+                    AsyncImage(url: URL(string: result.artworkUrl100)) { phase in
+                        if let image = phase.image {
+                            image
+                                .resizable()
+                                .scaledToFit()
+                        } else if phase.error != nil {
+                            Text("There was an error loading the image.")
+                        } else {
+                            ProgressView()
+                        }
+                    }
+                    .frame(width: 100, height: 100)
+                    .clipShape(.rect(cornerRadius: 16))
+                    
+                    VStack(alignment: .leading) {
+                        Text(result.trackName).fontDesign(.monospaced)
+                        Text(result.collectionName).font(.callout.bold())
+                    }
+                }
+            }
         }
-        .padding()
+        .task {
+             await fetchData()
+        }
+    }
+    
+    func fetchData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invaild URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+            
+        } catch {
+            print("Invalid Data")
+        }
     }
 }
 
